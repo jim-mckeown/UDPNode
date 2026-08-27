6462c7ceb5fa6552eca6f35716d823c37080c1ee
Jim McKeown <23134405+jim-mckeown@users.noreply.github.com>
2026-08-27 11:17:44 +0700

    Change version number in UDPNode.cpp
    Updated version number from 1.0.0 to 0.1.0.

--- UDPNode.cpp
+++ UDPNode.cpp
@@ -5,7 +5,7 @@
  * Encapsulates network packet parsing, LittleFS file read/write operations,
  * background Wi-Fi health monitoring, and NTP time synchronization routines.
  * 
- * @version 1.0.0
+ * @version 0.1.0
  * @date July 2026
  * @author Jim McKeown
  * @license MIT License
